c0aa826b6e49920d78407253441cfbd55b14bb39
RAGUNANDHA M <dreamerday097@gmail.com>
2026-08-12 10:23:05 +0530

    Time: 0 ms (100%), Space: 21.6 MB (10.16%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        c=Counter(nums)
+        for i in c:
+            if c[i]==1:
+                return i
