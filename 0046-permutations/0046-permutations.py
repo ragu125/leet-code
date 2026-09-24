417ee918f9344cfa3a53eed583ba9f8748f16a6f
RAGUNANDHA M <dreamerday097@gmail.com>
2026-09-24 13:53:37 +0530

    Time: 0 ms (100%), Space: 19.2 MB (99.56%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,6 +3,8 @@ class Solution:
     def permute(self, nums: list[int]) -> list[list[int]]:
         p=permutations(nums)
         res=[]
+
         for i in p:
             res.append(i)
+
         return res    
